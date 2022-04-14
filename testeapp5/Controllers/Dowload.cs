d92a9588553cebddd6ac42b36e7f5c555929e80e
Adrian Henrique <adrianhenriqueferreiraopo@gmail.com>
2022-04-14 15:10:13 -0400

    Adicionar arquivos de projeto.

---
+++ testeapp5/Controllers/Dowload.cs
@@ -0,0 +1,32 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace S3.Demo.API.Controllers;
+
+[Route("api/files")]
+[ApiController]
+public class Dowload : ControllerBase
+{
+    private readonly IAmazonS3 s3Dwl;
+    public Dowload(IAmazonS3 s3Donwload)
+    {
+        s3Dwl = s3Donwload;
+    }
+
+    [HttpGet("Donwload")]
+    public async Task<IActionResult> GetFileByKeyAsync(string bucketName, string archiveName)
+    {
+        //verificação
+        var verificacaoBck = await s3Dwl.DoesS3BucketExistAsync(bucketName);
+        if (!verificacaoBck) 
+        return NotFound($"Bucket {bucketName} não encontrado.");
+
+        
+        var s3archive = await s3Dwl.GetObjectAsync(bucketName, archiveName);
+        return File(s3archive.ResponseStream, s3archive.Headers.ContentType);
+    }
+}
+
+
